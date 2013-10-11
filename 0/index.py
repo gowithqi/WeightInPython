f1c45ad5d5670b58d4d844d1d44a124a9dcd466e
gowithqi <gowithqi@126.com>
2013-10-11 15:10:38 +0800

    lalala

--- 0/index.py
+++ 0/index.py
@@ -1,11 +1,25 @@
 #-*- coding:utf-8 -*-
 
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body=["Welcome to Baidu Cloud! I wanna fuc,k\n"]
-    return body
+# def app(environ, start_response):
+#     status = '200 OK'
+#     headers = [('Content-type', 'text/html')]
+#     start_response(status, headers)
+#     body=["Welcome to Baidu Cloud! I wanna fuc,k\n"]
+#     return body
 
+# from bae.core.wsgi import WSGIApplication
+# application = WSGIApplication(app)
+
+import os
+import sys
+ 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+ 
+path = os.path.dirname(os.path.abspath(__file__)) + '/mysite'
+if path not in sys.path:
+    sys.path.insert(1, path)
+ 
+from django.core.handlers.wsgi import WSGIHandler
 from bae.core.wsgi import WSGIApplication
-application = WSGIApplication(app)
+ 
+application = WSGIApplication(WSGIHandler())
