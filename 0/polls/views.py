3d685ff9f874ba4ac2dec3e5e2dd7011d0686f39
gowithqi <gowithqi@126.com>
2013-10-11 17:02:12 +0800

    9

--- 0/polls/views.py
+++ 0/polls/views.py
@@ -14,30 +14,30 @@ class IndexView(generic.ListView):
 	def get_queryset(self):
 	 	return Poll.objects.order_by('-pub_date')[:5]
 
-# class DetailView(generic.DetailView): 
-# 	model = Poll
-# 	template_name = 'polls/detail.html'
+class DetailView(generic.DetailView): 
+	model = Poll
+	template_name = 'polls/detail.html'
 
-# class ResultsView(generic.DetailView): 
-# 	model = Poll
-# 	template_name = "polls/results.html"
+class ResultsView(generic.DetailView): 
+	model = Poll
+	template_name = "polls/results.html"
 
 # def index(request):
 #     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
 #     # template = loader.get_template('polls/index.html')
 #     return HttpResponseRedirect(reverse('/static/html/polls/index.html', args=(latest_poll_list, )))
 
-def detail(request, poll_id):
-	try:
-		poll = Poll.objects.get(pk=poll_id)
-	except Poll.DoesNotExist:
-		raise Http404
-	return HttpResponseRedirect(reverse('/static/html/polls/detail.html', args=(poll, )))
+# def detail(request, poll_id):
+# 	try:
+# 		poll = Poll.objects.get(pk=poll_id)
+# 	except Poll.DoesNotExist:
+# 		raise Http404
+# 	return HttpResponseRedirect(reverse('/static/html/polls/detail.html', args=(poll, )))
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
+# def results(request, poll_id):
+#     poll = get_object_or_404(Poll, pk=poll_id)
     
-    return HttpResponseRedirect(reverse('/static/html/polls/results.html', args=(poll, )))
+#     return HttpResponseRedirect(reverse('/static/html/polls/results.html', args=(poll, )))
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
